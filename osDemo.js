97a19537a85fd1983c313576f92f00de9bfe0b17
amine <boughabaamine76@gmail.com>
2025-08-30 15:47:48 +0100

    exploring os module

---
+++ osDemo.js
@@ -0,0 +1,11 @@
+import os, { cpus, freemem, totalmem, userInfo } from "os";
+// userInfo()
+// console.log(os.userInfo());
+
+// totalmem()
+// console.log(os.totalmem());
+// freemem()
+// console.log(os.freemem());
+
+// cpus()
+console.log(os.cpus());
