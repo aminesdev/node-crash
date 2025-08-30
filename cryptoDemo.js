5a979c423a4fc3f82b93f002bfff00683b1764eb
amine <boughabaamine76@gmail.com>
2025-08-30 16:27:57 +0100

    AES-256-CBC Encryption/Decryption

--- cryptoDemo.js
+++ cryptoDemo.js
@@ -1,13 +1,31 @@
-import { createHash, randomBytes } from "crypto";
-//createHash()
-const hash = createHash("sha256");
-hash.update("password123");
-console.log(hash.digest("hex"));
-//randomBytes()
-randomBytes(16, (err, buf) => {
-    if (err) throw err;
-    console.log(buf.toString("hex"));
-});
+const crypto = require("crypto");
 
-//createCipheriv & createDecipheriv
+// Algorithm + Key + IV
+const algorithm = "aes-256-cbc";
+const key = crypto.randomBytes(32); // 32 bytes for AES-256
+const iv = crypto.randomBytes(16); // 16 bytes for CBC mode
 
+// Encrypt function
+function encrypt(text) {
+    const cipher = crypto.createCipheriv(algorithm, key, iv);
+    let encrypted = cipher.update(text, "utf8", "hex");
+    encrypted += cipher.final("hex");
+    return encrypted;
+}
+
+// Decrypt function
+function decrypt(encryptedText) {
+    const decipher = crypto.createDecipheriv(algorithm, key, iv);
+    let decrypted = decipher.update(encryptedText, "hex", "utf8");
+    decrypted += decipher.final("utf8");
+    return decrypted;
+}
+
+// Usage
+const message = "Hello, secure world!";
+const encrypted = encrypt(message);
+const decrypted = decrypt(encrypted);
+
+console.log("Original:", message);
+console.log("Encrypted:", encrypted);
+console.log("Decrypted:", decrypted);
