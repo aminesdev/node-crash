64992fdea0132eb6f87a855da8bdf8719f807235
amine <boughabaamine76@gmail.com>
2025-08-30 18:21:29 +0100

    discovering some other process stuff

--- processDemo.js
+++ processDemo.js
@@ -3,4 +3,24 @@
 // console.log(process.argv[3]);
 
 // process.env
-console.log(process.env.LOGNAME);
+// console.log(process.env.LOGNAME);
+
+//pid
+// console.log(process.cwd());
+
+//title
+// console.log(process.title);
+
+// memoryUsage
+// console.log(process.memoryUsage());
+
+//uptime()
+// console.log(process.uptime());
+
+process.on('exit', (code) => {
+    console.log("about to exite with code 0")
+})
+
+//exit()
+process.exit(0);
+console.log("hello from after exite()")
